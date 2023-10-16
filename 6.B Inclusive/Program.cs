49282971d9ed169ce1b27fa27b737a8ae68a1130
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-16 14:44:24 +0200

    Add project files.

---
+++ 6.B Inclusive/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.B_Inclusive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(11, 20, 12));
+            Console.WriteLine(test(30, 30, 17));
+            Console.WriteLine(test(25, 35, 50));
+            Console.WriteLine(test(15, 12, 8));
+            Console.ReadKey();
+        }
+        public static bool test(int a, int b, int c)
+        {
+            return (a  >= 20 && a <= 50) || (b >= 20 && b <= 50) || (c >= 20 && c <= 50);
+            
+        }
+
+    }
+}
